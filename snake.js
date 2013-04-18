95c3e38b505d9bc673ab320007f131be481919b3
App Academy Student <student@appacademy.io>
2013-04-17 22:21:48 -0700

    add ui

--- snake.js
+++ snake.js
@@ -1,27 +1,40 @@
 var Board = function(size){
 	this.grid = _.times(size, function() {return Array(size);});
-	for(var i = 0; i < size; i++){
-		for(var j = 0; j < size; j++){
-			this.grid[i][j] = ' ';
-		}
-	}
+
 	this.size = size;
-	this.grid[5][5] = 'X';
+
 	this.validMove = function(move) { 
-		return(this.grid[move[0]][move[1]] != "S" || (move[0] > 0 && move[0] < size) || 
-			(move[1] > 0 && move[1] < size))
+		return(this.grid[move[0]][move[1]] != "S" && (move[0] >= 0 && move[0] <= size) && 
+			(move[1] >= 0 && move[1] <= size))
 	};
 
-	this.makeMove = function(move) {
+	this.makeMove = function(move, snake) {
+		this.clearBoard();
+		this.grid[this.apple[0]][this.apple[1]] = "A";
+		console.log(snake.history.length);
+		for(i = 0; i < snake.history.length; i++) {
+			this.grid[snake.history[i][0]][snake.history[i][1]] = "B"
+		};
 		this.grid[move[0]][move[1]] = "S";
 	};
+
+	this.clearBoard = function() {
+		for(var i = 0; i < size; i++) {
+			for(var j = 0; j < size; j++) {
+				this.grid[i][j] = ' ';
+			}
+		};
+	};
+
+	this.apple = [];
 
 	this.makeApple = function() {
 		while(true) {
 			var x = Math.floor(Math.random() * size);
 			var y = Math.floor(Math.random() * size);
-			if (this.grid[x][y] === undefined) {
+			if (this.grid[x][y] != "S" && this.grid[x][y] != "B" ) {
 				this.grid[x][y] = "A";
+				this.apple = [[x],[y]];
 				break;
 			}
 		}
@@ -30,8 +43,8 @@ var Board = function(size){
 };
 
 var Snake = function() {
-	this.upVector = [1,0];
-	this.downVector = [-1,0];
+	this.downVector = [1,0];
+	this.upVector = [-1,0];
 	this.rightVector = [0,1];
 	this.leftVector = [0,-1];
 
@@ -41,9 +54,27 @@ var Snake = function() {
 
 	this.currentSquare = [5,5]
 
+	this.turn = function(num) {
+		switch(num) {
+			case 37:
+				this.direction = this.leftVector;
+				break;
+			case 38: 
+				this.direction = this.upVector;
+				break;
+			case 39:
+				this.direction = this.rightVector;
+				break;
+			case 40:
+				this.direction = this.downVector;
+				break;
+		}
+	};
+
 	this.move = function() {
 		var newPos = [this.direction[0] + this.currentSquare[0],
 									this.direction[1] + this.currentSquare[1]];
+		this.currentSquare = newPos
 		return newPos;
 	};
 
@@ -52,24 +83,25 @@ var Snake = function() {
 	this.update = function(board, move) {
 		if(board.grid[move[0]][move[1]] === "A"){
 				this.children += 1;
+				board.makeApple();
 			}else{
 				this.history.pop();
 			}
-			this.history.push(move);
+			this.history.unshift(move);
 	};
 
 };
 
 
-var Game = function() {
-	this.board = Board.new;
-	this.snake = Snake.new;
+var Game = function(size) {
+	this.board = new Board(size);
+	this.snake = new Snake;
 	
 	this.step = function() {
-		var move = snake.move();
-		if(validMove(move)){
-			snake.update(this.board, move);
-			makeMove(move);
+		var move = this.snake.move();
+		if(this.board.validMove(move)){
+			this.snake.update(this.board, move); //grows snake, updates apple location if hit
+			this.board.makeMove(move, this.snake);
 		}
 	};
 
--- snake_ui.js
+++ snake_ui.js
@@ -1,14 +1,44 @@
 var render = function(board) {
-	for(var i = 0; i < board.size; i++){
-		for(var j = 0; j < board.size; j++){
-			$('pre').text(board.grid[i][j]);			
+	$('.game').empty()
+	for(var i = 0; i < board.grid.length; i++){
+		for(var j = 0; j < board.grid.length; j++){
+			if(board.grid[i][j] === "S") {
+				$('.game').append('<div class="board" id="S"></div>');
+			} else if(board.grid[i][j] === "B") {
+				$('.game').append('<div class="board" id="B"></div>');
+			} else if(board.grid[i][j] === "A") {
+				$('.game').append('<div class="board" id="A"></div>');
+			} else {	
+			$('.game').append('<div class="board" id="' + i + '-' +  j +'"">' + board.grid[i][j] + '</div>');
+			}
 		}
 	}
 };
 
+var listen = function(snake) {
+	$('html').keydown(function (event) {
+		snake.turn(event.keyCode);
+	  console.log("You pressed keycode: " + event.keyCode);
+	});
+};
+
+
+
 $(document).ready(function(){
-	 var b = new Board(10);
-	 console.log(b.grid);
-	 render(b);
+	 var g = new Game(10);
+	 g.board.clearBoard();
+	 g.board.makeApple();
+	 render(g.board);
+
+	 listen(g.snake);
+
+	 var play = function() {
+	 	render(g.board);
+	 	g.step();	
+	 	window.setTimeout(play, 100);
+	 };
+
+	 
+	 window.setTimeout(play, 100);
 
 });
